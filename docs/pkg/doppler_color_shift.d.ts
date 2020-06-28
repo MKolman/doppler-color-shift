ed1c170bbb090621d65d40ad03af88929d14cbf6
Maks Kolman <makskolman@google.com>
2020-06-28 22:57:52 +0100

    Create preview page in docs/

---
+++ docs/pkg/doppler_color_shift.d.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {CanvasRenderingContext2D} ctx 
+* @param {number} velocity 
+*/
+export function color_shift_canvas(ctx: CanvasRenderingContext2D, velocity: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly color_shift_canvas: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+        
